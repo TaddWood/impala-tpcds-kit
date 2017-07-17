cebe2bab9f0c2a9849d1fa1fabc52dc3125552fd
Tadd Wood <tadd@arcadiadata.com>
2017-07-17 15:04:01 -0700

    Enable processes running on date nodes to use ssh key config
    push-bits.sh, set-nodenum.sh, and run-gen-facts.sh all attempt to either push information or run a process across the data nodes.  If you're running a system that doesn't have keyless ssh access between nodes, this code will fail until now.  In each of these files we now check our newly added SSH config section in tpcds-env.sh, and if a ssh config is set up (SSH_KEY=True, SSH_KEYNAME=<yourkeyname>), you will now be able to run these processes across date nodes that share the ssh key specified.

--- push-bits.sh
+++ push-bits.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+source tpcds-env.sh
 
+# Check SSH Config settings from tpcds-env.sh
+
+echo "SSH_KEY env variable is set to $SSH_KEY"
+
+if [ "$SSH_KEY" = "False" ]; then
+  echo "SSH keyless access between nodes is assumed to be enabled. SSH config will be ignored."
+else
+  echo "SSH key required for node access. Using SSH config from tpcds-env.sh."
+fi
+
+# Push tpcds kits (tpcds-kit, impala-tpcds-kit) to each data node specified in dn.txt
 # TODO: make sure you have set up dn.txt with your DataNode hostnames, 1 per line
-set -e
 
 cat dn.txt | while read h
 do
-  scp -rp $HOME/tpcds-kit $h:$HOME
-  scp -rp $HOME/impala-tpcds-kit $h:$HOME
+  if [ "$SSH_KEY" = "False" ]; then
+    echo "scp -rp $HOME/tpcds-kit $h:$HOME"
+    scp -rp $HOME/tpcds-kit $h:$HOME
+    echo "scp -rp $HOME/impala-tpcds-kit $h:$HOME"
+    scp -rp $HOME/impala-tpcds-kit $h:$HOME
+  else
+    echo "scp -i $HOME/.ssh/$SSH_KEYNAME -rp $HOME/tpcds-kit $h:$HOME"
+    scp -i $HOME/.ssh/$SSH_KEYNAME -rp $HOME/tpcds-kit $h:$HOME
+    echo "scp -i $HOME/.ssh/$SSH_KEYNAME -rp $HOME/impala-tpcds-kit $h:$HOME"
+    scp -i $HOME/.ssh/$SSH_KEYNAME -rp $HOME/impala-tpcds-kit $h:$HOME
+  fi
 done
--- set-nodenum.sh
+++ set-nodenum.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+source tpcds-env.sh
 
+# Check SSH Config settings from tpcds-env.sh
+
+echo "SSH_KEY env variable is set to $SSH_KEY"
+
+if [ "$SSH_KEY" = "False" ]; then
+  echo "SSH keyless access between nodes is assumed to be enabled. SSH config will be ignored."
+else
+  echo "SSH key required for node access. Using SSH config from tpcds-env.sh."
+fi
+
+# Set node number for each data node specified in dn.txt
 # TODO: make sure you have set up dn.txt with your DataNode hostnames, 1 per line
 
 n=1
 cat dn.txt | while read h
-do 
+do
   echo "$h = $n"
-  ssh $h "echo export NODENUM=${n} > $HOME/impala-tpcds-kit/nodenum.sh" < /dev/null
+  if [ "$SSH_KEY" = "False" ]; then
+    echo "$h 'echo export NODENUM=${n} > $HOME/impala-tpcds-kit/nodenum.sh' < /dev/null"
+    ssh $h "echo export NODENUM=${n} > $HOME/impala-tpcds-kit/nodenum.sh" < /dev/null
+  else
+    echo "ssh -i $HOME/.ssh/$SSH_KEYNAME $h 'echo export NODENUM=${n} > $HOME/impala-tpcds-kit/nodenum.sh' < /dev/null"
+    ssh -i $HOME/.ssh/$SSH_KEYNAME $h "echo export NODENUM=${n} > $HOME/impala-tpcds-kit/nodenum.sh" < /dev/null
+  fi
   ((n=n+1))
 done
